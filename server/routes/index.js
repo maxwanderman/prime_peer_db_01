69c00c265f3ad1cb2a7864750b021e375d237640
Erika Klein <ekmpls@gmail.com>
2016-04-11 19:34:13 -0500

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
-var router = express.Router();
 var path = require('path');
+var router = express.Router();
 
 router.get('/', function(request, response){
   response.sendFile(path.join(__dirname, '../public/views/index.html'));
-})
+});
 
 module.exports = router;
+
